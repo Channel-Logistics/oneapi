0c1b8311765753ad5ca61c1c9d193130e15e4531
Esteban Sanchez <esteban@space-eyes.com>
2025-09-15 16:37:39 -0500

    fix: remove duplicate import of OrderProviderRead in contracts initialization

--- packages/oneapi-contracts/src/contracts/__init__.py
+++ packages/oneapi-contracts/src/contracts/__init__.py
@@ -1,9 +1,9 @@
-from .order_provider import OrderProviderRead, OrderProviderCreate, OrderProviderRead, OrderProviderUpdate
+from .order_provider import  OrderProviderRead, OrderProviderCreate, OrderProviderUpdate
 from .providers import ProviderCreate, ProviderBase, ProviderRead, ProviderUpdate
 from .orders import  OrderCreate, OrderRead, OrderUpdate, OrderStatus
 
 __all__ = [
-    "OrderProviderRead", "OrderProviderCreate", "OrderProviderRead", "OrderProviderUpdate",
+    "OrderProviderRead", "OrderProviderCreate", "OrderProviderUpdate",
     "ProviderCreate", "ProviderBase", "ProviderRead", "ProviderUpdate",
     "OrderCreate", "OrderRead", "OrderUpdate", "OrderStatus",
 ]
